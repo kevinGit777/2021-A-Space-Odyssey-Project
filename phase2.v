99c1b495420eef586abc9f8a0c7e235f76519654
Kim-Nhi Ngo <goldenchildngo@gmail.com>
2021-10-12 00:57:33 -0500

    addsub structured code

--- phase2.v
+++ phase2.v
@@ -1,19 +1,106 @@
-//Adder-subtractor module
-module addsub (num1, num2, op, sum);
-	input [15:0] num1;
-	input [15:0] num2;
-	input [3:0] op;
-	output reg [31:0] sum;
+module HalfAdder(A,B,carry,sum);
+	input A;
+	input B;
+	output carry;
+	output sum;
+	reg carry;
+	reg sum;
 
-    reg [31:0] t1;
+	always @(*) begin
+		sum = A ^ B;
+		carry = A & B;
+	end
+endmodule
 
-	always @(num1, num2, op) begin
-		if(op == 0)
-            t1 = num1 + num2;
-		else
-            t1 = num1 - num2;
-        sum = t1;
-    end
+
+module FullAdder(A,B,C,carry,sum);
+	input A;
+	input B;
+	input C;
+	output carry;
+	output sum;
+	reg carry;
+	reg sum;
+
+	wire c0;
+	wire s0;
+	wire c1;
+	wire s1;
+
+	HalfAdder ha1(A,B,c0,s0);
+	HalfAdder ha2(s0,C,c1,s1);
+
+	always @(*) begin
+		sum = s1;
+		sum = A ^ B ^ C;
+		carry = c1 | c0;
+		carry = ((A ^ B) & C) | (A & B);
+	end
+endmodule
+
+
+module addsub (inputA, inputB, mode, carry, sum, overflow);
+    input [15:0] inputA;
+    input [15:0] inputB;
+    input [3:0] mode;
+    output [31:0] sum;
+    output carry;
+    output overflow;
+    wire b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15;
+    wire c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16;
+    assign m = mode[0];
+
+    assign b0 = inputB[0] ^ m;
+    assign b1 = inputB[1] ^ m;
+    assign b2 = inputB[2] ^ m;
+    assign b3 = inputB[3] ^ m;
+    assign b4 = inputB[4] ^ m;
+    assign b5 = inputB[5] ^ m;
+    assign b6 = inputB[6] ^ m;
+    assign b7 = inputB[7] ^ m;
+    assign b8 = inputB[8] ^ m;
+    assign b9 = inputB[9] ^ m;
+    assign b10 = inputB[10] ^ m;
+    assign b11 = inputB[11] ^ m;
+    assign b12 = inputB[12] ^ m;
+    assign b13 = inputB[13] ^ m;
+    assign b14 = inputB[14] ^ m;
+    assign b15 = inputB[15] ^ m;
+
+    FullAdder FA0(inputA[0],b0,m,c1,sum[0]);
+    FullAdder FA1(inputA[1],b1,  c1,c2,sum[1]);
+    FullAdder FA2(inputA[2],b2,  c2,c3,sum[2]);
+    FullAdder FA3(inputA[3],b3,  c3,c4,sum[3]);
+    FullAdder FA4(inputA[4],b4, c4,c5,sum[4]);
+    FullAdder FA5(inputA[5],b5,  c5,c6,sum[5]);
+    FullAdder FA6(inputA[6],b6,  c6,c7,sum[6]);
+    FullAdder FA7(inputA[7],b7,  c7,c8,sum[7]);
+    FullAdder FA8(inputA[8],b8, c8,c9,sum[8]);
+    FullAdder FA9(inputA[9],b9,  c9,c10,sum[9]);
+    FullAdder FA10(inputA[10],b10,  c10,c11,sum[10]);
+    FullAdder FA11(inputA[11],b11,  c11,c12,sum[11]);
+    FullAdder FA12(inputA[12],b12, c12,c13,sum[12]);
+    FullAdder FA13(inputA[13],b13,  c13,c14,sum[13]);
+    FullAdder FA14(inputA[14],b14,  c14,c15,sum[14]);
+    FullAdder FA15(inputA[15],b15,  c15,c16,sum[15]);
+    assign sum[16] = c16 ^ m;
+    assign sum[17] = c16 ^ m;
+    assign sum[18] = c16 ^ m;
+    assign sum[19] = c16 ^ m;
+    assign sum[20] = c16 ^ m;
+    assign sum[21] = c16 ^ m;
+    assign sum[22] = c16 ^ m;
+    assign sum[23] = c16 ^ m;
+    assign sum[24] = c16 ^ m;
+    assign sum[25] = c16 ^ m;
+    assign sum[26] = c16 ^ m;
+    assign sum[27] = c16 ^ m;
+    assign sum[28] = c16 ^ m;
+    assign sum[29] = c16 ^ m;
+    assign sum[30] = c16 ^ m;
+    assign sum[31] = c16 ^ m;
+    assign carry=c15;
+    assign overflow=c16^c15;
 endmodule
 
 //Modulus module
@@ -209,12 +296,13 @@ module BreadBoard (
     wire [31:0] product;
     wire [31:0] quotient;
     wire [31:0] remainder;
+    wire carry;
     wire err_0;
     wire err_1;
 
 
 
-    addsub as(input1, input2, op_code, sum);
+    addsub as(input1, input2, op_code, carry, sum, err_0);
     multiplication mul(input1, input2, product);
     divide dv(input1, input2, quotient, err_1);
     modulo mod(input1, input2, remainder, err_1);
