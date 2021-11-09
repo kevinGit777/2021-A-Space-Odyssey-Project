8d9adc7c81ea8a226801a22569fda90c7432e4eb
kevinGit777 <57700273+kevinGit777@users.noreply.github.com>
2021-11-08 21:44:19 -0600

    Update usefile.py

--- usefile.py
+++ usefile.py
@@ -4,9 +4,11 @@ import subprocess
 f = open("inpipe.txt", 'w')
 f.writelines("1000")
 f.close()
-#subprocess.run(["./file"])
+
 os.system('iverilog readfile.v -o file')
 os.system('./file')
 f = open("outpipe.txt", 'r')
 
+#read result form verilog
 print("new value is: " + f.readline())
+f.close()
