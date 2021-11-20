2cbdbd4819a2b506ba8cd62f2d0f47df79c7737d
kevinGit777 <kevinwu0316@gmail.com>
2021-11-19 23:40:28 -0600

    add one rainbow color

--- Space Odyssey.Part4.v
+++ Space Odyssey.Part4.v
@@ -32,17 +32,17 @@ module Testbench (
     initial begin
 
     op_code = 4'b0001;
-    #10;
     j = 0;
     for (i = 0; i < 10 ; i++) 
     begin
+        #10;
         $display("OP_CODE:%d, Iteration %2d", op_code, i+1);
         $display ("[%d, %d, %d]", strip[0][23:16], strip[0][15:8], strip[0][7:0]);
         $display ("[%d, %d, %d]", strip[1][23:16], strip[1][15:8], strip[1][7:0]);       
         $display ("[%d, %d, %d]", strip[2][23:16], strip[2][15:8], strip[2][7:0]);
         $display ("[%d, %d, %d]", strip[3][23:16], strip[3][15:8], strip[3][7:0]);
         $display ("[%d, %d, %d]", strip[4][23:16], strip[4][15:8], strip[4][7:0]);
-        #10;
+        
 /*        $display ("[%d, %d, %d]", strip[5][23:16], strip[5][15:8], strip[5][7:0]);
         $display ("[%d, %d, %d]", strip[6][23:16], strip[6][15:8], strip[6][7:0]);
         $display ("[%d, %d, %d]", strip[7][23:16], strip[7][15:8], strip[7][7:0]);
@@ -52,7 +52,7 @@ module Testbench (
     end
     
     
-    op_code = 4'b0000;
+    op_code = 4'b0000; // back to no_op 
     op_code = 4'b0010;
     #5;
     #5;
@@ -137,15 +137,15 @@ module BreadBoard (
     wire [2:0] color_mux_out;
     wire [9:0][23:0] strip_out;
     
-    Brightness_mux Mux1(op_code, brightness_mux_out, clk);
-   // Mode_mux Mux2(op_code, mode_mux_out, clk);
-    Color_mux Mux3(op_code, color_mux_out, clk);
+    Brightness_mux Mux1(op_code, brightness_mux_out );
+   Mode_mux Mux2(op_code, mode_mux_out );
+    Color_mux Mux3(op_code, color_mux_out);
     Strip_mux Mux4(op_code, mode, color_code, clk, strip_out);
    // DFF f (clk);
 
     assign brightness = brightness_mux_out;
-    //assign mode = mode_mux_out;
-    assign mode = 2;
+    assign mode = mode_mux_out;
+    //assign mode = 2;
     assign color_code = color_mux_out;
     assign strip = strip_out;
 
@@ -158,18 +158,42 @@ module DFF(clk);
 endmodule
 
 module Brightness_mux (
-    op_code, brightness_mux_out, clk
+    op_code, brightness_mux_out
 );
 input  [3:0] op_code;
-input clk;
 output reg [2:0] brightness_mux_out;
 
-wire [3:0] channels;  
-assign channels[3] = 15;
+reg [3:0][2:0] channels;  
+
+initial begin
+    channels[3] = 0;
+end
+
 
 always @(*) begin
     if (op_code == 1) begin
         brightness_mux_out = channels[3]; // default/initial value
+    end
+    else if (op_code == 6) begin
+        brightness_mux_out = channels[1];
+    end 
+    else if(op_code == 7) begin
+        brightness_mux_out = channels[2];
+    end
+    else begin
+       brightness_mux_out = channels[0];
+    end
+
+    channels[0] = brightness_mux_out;
+    
+    channels[1] = brightness_mux_out + 1;
+    if(brightness_mux_out == 15) begin
+      channels[1] = 0;
+    end
+
+    channels[2] = brightness_mux_out - 1;
+    if (brightness_mux_out == 0) begin
+        channels[2] = 15;
     end
 end
 
@@ -177,10 +201,9 @@ end
 endmodule
 
 module Mode_mux (
-    op_code, mode_mux_out, clk
+    op_code, mode_mux_out
 );
 input  [3:0] op_code;
-input clk;
 output reg [1:0] mode_mux_out;
 
 reg [3:0][1:0] channels;  
@@ -215,29 +238,50 @@ always @(*) begin
     if (mode_mux_out == 0) begin
         channels[2] = 2;
     end
-   // $display("Mode mux out: %d", mode_mux_out);
-    //$display("Mode channel 1: %d", channels[1]);
 
 end
 
 endmodule
 
 module Color_mux (
-    op_code, color_mux_out, clk
+    op_code, color_mux_out
 );
 
 input  [3:0] op_code;
-input clk;
 output reg [2:0] color_mux_out;
 
-wire [3:0] channels;  
-assign channels[3] = 0;
+reg [3:0][2:0] channels;  
 
-always @(*) begin
+initial begin
+    channels[3] = 0;
+end
+
+
+always @(op_code) begin
     if (op_code == 1) begin
         color_mux_out = channels[3]; // default/initial value
+        channels[2] = color_mux_out; 
     end
+    else if (op_code == 4) begin
+        color_mux_out = channels[1];
+        channels[2] = channels[0]; 
+    end 
+    else if(op_code == 5) begin
+        color_mux_out = channels[2];
+    end
+    else begin
+       color_mux_out = channels[0];
+    end
+    
+    channels[0] = color_mux_out;
+    
+    channels[1] = color_mux_out + 1;
+    if(color_mux_out == 15) begin
+      channels[1] = 0;
+    end
+
 end
+
     
 endmodule
 
@@ -341,6 +385,18 @@ module get_rainbow_color (
         colors[1][7] = 24'b01010010_00111010_00110100;
         colors[1][8] = 24'b10111001_11111010_11111000;
         colors[1][9] = 24'b01001001_00110000_01101011;
+
+        colors[2][0] = 'h947A4A;
+        colors[2][1] = 'hE09816;
+        colors[2][2] = 'hE05961;
+        colors[2][3] = 'h432C94;
+        colors[2][4] = 'h5831E0;
+        colors[2][5] = 'h947A4A;
+        colors[2][6] = 'hE09816;
+        colors[2][7] = 'hE05961;
+        colors[2][8] = 'h432C94;
+        colors[2][9] = 'h5831E0;
+        
     end
 
     always @(*) begin
