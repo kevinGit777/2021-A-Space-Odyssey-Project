c1b160b9cbb3765792022df9f61147436826f837
kevinGit777 <kevinwu0316@gmail.com>
2021-11-20 02:08:01 -0600

    finish all

--- Space Odyssey.Part4.v
+++ Space Odyssey.Part4.v
@@ -1,14 +1,9 @@
-//=============================================
-// DFF
-//=============================================
-
 module Testbench (
 );
     wire [9:0][23:0] strip;
     reg [3:0] op_code;
     reg clk;
     reg [4:0] i;
-    reg [4:0] j;
     wire [2:0] brightness;
 
     BreadBoard BB(
@@ -17,8 +12,6 @@ module Testbench (
     .strip(strip),
     .brightness(brightness)
     );
-
-
 
     initial begin
         forever begin
@@ -31,88 +24,82 @@ module Testbench (
 
     initial begin
 
-    op_code = 4'b0001;
-    j = 0;
-    for (i = 0; i < 10 ; i++) 
-    begin
-        #10;
-        $display("OP_CODE:%d, Iteration %2d", op_code, i+1);
-        $display ("[%d, %d, %d]", strip[0][23:16], strip[0][15:8], strip[0][7:0]);
-        $display ("[%d, %d, %d]", strip[1][23:16], strip[1][15:8], strip[1][7:0]);       
-        $display ("[%d, %d, %d]", strip[2][23:16], strip[2][15:8], strip[2][7:0]);
-        $display ("[%d, %d, %d]", strip[3][23:16], strip[3][15:8], strip[3][7:0]);
-        $display ("[%d, %d, %d]", strip[4][23:16], strip[4][15:8], strip[4][7:0]);
-        
-/*        $display ("[%d, %d, %d]", strip[5][23:16], strip[5][15:8], strip[5][7:0]);
-        $display ("[%d, %d, %d]", strip[6][23:16], strip[6][15:8], strip[6][7:0]);
-        $display ("[%d, %d, %d]", strip[7][23:16], strip[7][15:8], strip[7][7:0]);
-        $display ("[%d, %d, %d]", strip[8][23:16], strip[8][15:8], strip[8][7:0]);
-        $display ("[%d, %d, %d]", strip[9][23:16], strip[9][15:8], strip[9][7:0]);
-*/
-    end
-    
-    
-    op_code = 4'b0000; // back to no_op 
-    op_code = 4'b0010;
-    #5;
-    #5;
-    for (i = 0; i < 10 ; i++) 
-    begin
-/*        $display("OP_CODE:%d, Iteration %2d", op_code, i+1);
-        $display ("[%d, %d, %d]", strip[0][23:16], strip[0][15:8], strip[0][7:0]);
-        $display ("[%d, %d, %d]", strip[1][23:16], strip[1][15:8], strip[1][7:0]);
-        $display ("[%d, %d, %d]", strip[2][23:16], strip[2][15:8], strip[2][7:0]);
-        $display ("[%d, %d, %d]", strip[3][23:16], strip[3][15:8], strip[3][7:0]);
-        $display ("[%d, %d, %d]", strip[4][23:16], strip[4][15:8], strip[4][7:0]);
-        $display ("[%d, %d, %d]", strip[5][23:16], strip[5][15:8], strip[5][7:0]);
-        $display ("[%d, %d, %d]", strip[6][23:16], strip[6][15:8], strip[6][7:0]);
-        $display ("[%d, %d, %d]", strip[7][23:16], strip[7][15:8], strip[7][7:0]);
-        $display ("[%d, %d, %d]", strip[8][23:16], strip[8][15:8], strip[8][7:0]);
-        $display ("[%d, %d, %d]", strip[9][23:16], strip[9][15:8], strip[9][7:0]);
- */   end
-    
-    op_code = 4'b0000;
-    op_code = 4'b0010;
-    
-    for (i = 0; i < 10 ; i++) 
-    begin
-        $display("OP_CODE:%d, Iteration %2d", op_code, i+1);
-        $display ("[%d, %d, %d]", strip[0][23:16], strip[0][15:8], strip[0][7:0]);
-        $display ("[%d, %d, %d]", strip[1][23:16], strip[1][15:8], strip[1][7:0]);
-        $display ("[%d, %d, %d]", strip[2][23:16], strip[2][15:8], strip[2][7:0]);
-        #10;
-/*        $display ("[%d, %d, %d]", strip[3][23:16], strip[3][15:8], strip[3][7:0]);
-        $display ("[%d, %d, %d]", strip[4][23:16], strip[4][15:8], strip[4][7:0]);
-        $display ("[%d, %d, %d]", strip[5][23:16], strip[5][15:8], strip[5][7:0]);
-        $display ("[%d, %d, %d]", strip[6][23:16], strip[6][15:8], strip[6][7:0]);
-        $display ("[%d, %d, %d]", strip[7][23:16], strip[7][15:8], strip[7][7:0]);
-        $display ("[%d, %d, %d]", strip[8][23:16], strip[8][15:8], strip[8][7:0]);
-        $display ("[%d, %d, %d]", strip[9][23:16], strip[9][15:8], strip[9][7:0]);
-        
- */   end
+    op_code = 4'b0001; //Turn On System
+    #100; 
+    op_code = 4'b0000; // back to no_op
 
-    op_code = 4'b0000;
-    //op_code = 4'b0010;
-    #5;
-    #5;
-    for (i = 0; i < 10 ; i++) 
-    begin
-        $display("OP_CODE:%d, Iteration %2d", op_code, i+1);
-        $display ("[%d, %d, %d]", strip[0][23:16], strip[0][15:8], strip[0][7:0]);
-/*        $display ("[%d, %d, %d]", strip[1][23:16], strip[1][15:8], strip[1][7:0]);
-        $display ("[%d, %d, %d]", strip[2][23:16], strip[2][15:8], strip[2][7:0]);
-        $display ("[%d, %d, %d]", strip[3][23:16], strip[3][15:8], strip[3][7:0]);
-        $display ("[%d, %d, %d]", strip[4][23:16], strip[4][15:8], strip[4][7:0]);
-        $display ("[%d, %d, %d]", strip[5][23:16], strip[5][15:8], strip[5][7:0]);
-        $display ("[%d, %d, %d]", strip[6][23:16], strip[6][15:8], strip[6][7:0]);
-        $display ("[%d, %d, %d]", strip[7][23:16], strip[7][15:8], strip[7][7:0]);
-        $display ("[%d, %d, %d]", strip[8][23:16], strip[8][15:8], strip[8][7:0]);
-        $display ("[%d, %d, %d]", strip[9][23:16], strip[9][15:8], strip[9][7:0]);
-   */ end
+    op_code = 4'b0010; //To Flashing Color
+    #100;
+    op_code = 4'b0000; // back to no_op
     
+    op_code = 4'b0010; // To rainbow
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0010; //Test mode Overflow
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0010; //Test mode Overflow
+    #100;
+    op_code = 4'b0000; // back to no_op
+/*
+    op_code = 4'b0100; //Next color
+    #100;
+    op_code = 4'b0000; // back to no_op
     
-    
-    //TODO: add other testing
+    op_code = 4'b0100; //Next color
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0100; //Next color
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0100; //Next color
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0101; //Prev color
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0101; //Prev color, will have same result as last one because we only memerize one last color.
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0110; //Bri up
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0110; //Bri up
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0111; //Bri down 
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0111; //Bri down 
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b0111; //Bri down 
+    #100;
+    op_code = 4'b0000; // back to no_op
+*/
+
+    op_code = 4'b1111; //set to color 8
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b1101; //set to color 6
+    #100;
+    op_code = 4'b0000; // back to no_op
+
+    op_code = 4'b1100; //set to color 5
+    #100;
+    op_code = 4'b0000; // back to no_op
     
     $finish;
     end
@@ -126,11 +113,12 @@ module BreadBoard (
 
     output [9:0][23:0] strip;
     output [2:0] brightness;
-    input [3:0] op_code;
+    input  [3:0] op_code;
     input clk;
 
     wire [1:0] mode;
     wire [2:0] color_code;
+    reg [4:0] i;
 
     wire [2:0] brightness_mux_out;
     wire [1:0] mode_mux_out;
@@ -138,16 +126,35 @@ module BreadBoard (
     wire [9:0][23:0] strip_out;
     
     Brightness_mux Mux1(op_code, brightness_mux_out );
-   Mode_mux Mux2(op_code, mode_mux_out );
+    Mode_mux Mux2(op_code, mode_mux_out );
     Color_mux Mux3(op_code, color_mux_out);
     Strip_mux Mux4(op_code, mode, color_code, clk, strip_out);
-   // DFF f (clk);
 
     assign brightness = brightness_mux_out;
     assign mode = mode_mux_out;
-    //assign mode = 2;
     assign color_code = color_mux_out;
     assign strip = strip_out;
+
+    always @(op_code) begin
+        #10;
+        $display("OP_CODE:%d, Mode:%d, Color Set:%d, Brightness:%d.", op_code, mode, color_code, brightness);
+    for (i = 0; i < 5 ; i++) 
+        begin
+        #10;
+        $display("Iteration %2d", i+1);
+        $display ("[%d, %d, %d]", strip[0][23:16], strip[0][15:8], strip[0][7:0]);
+        $display ("[%d, %d, %d]", strip[1][23:16], strip[1][15:8], strip[1][7:0]);       
+        $display ("[%d, %d, %d]", strip[2][23:16], strip[2][15:8], strip[2][7:0]);
+        $display ("[%d, %d, %d]", strip[3][23:16], strip[3][15:8], strip[3][7:0]);
+        $display ("[%d, %d, %d]", strip[4][23:16], strip[4][15:8], strip[4][7:0]);       
+        $display ("[%d, %d, %d]", strip[5][23:16], strip[5][15:8], strip[5][7:0]);
+        $display ("[%d, %d, %d]", strip[6][23:16], strip[6][15:8], strip[6][7:0]);
+        $display ("[%d, %d, %d]", strip[7][23:16], strip[7][15:8], strip[7][7:0]);
+        $display ("[%d, %d, %d]", strip[8][23:16], strip[8][15:8], strip[8][7:0]);
+        $display ("[%d, %d, %d]", strip[9][23:16], strip[9][15:8], strip[9][7:0]);
+        end
+        $display("\n");
+    end
 
 endmodule
 
@@ -166,7 +173,7 @@ output reg [2:0] brightness_mux_out;
 reg [3:0][2:0] channels;  
 
 initial begin
-    channels[3] = 0;
+    channels[3] = 7;
 end
 
 
@@ -187,13 +194,13 @@ always @(*) begin
     channels[0] = brightness_mux_out;
     
     channels[1] = brightness_mux_out + 1;
-    if(brightness_mux_out == 15) begin
-      channels[1] = 0;
+    if(brightness_mux_out == 7) begin
+      channels[1] = 7;
     end
 
     channels[2] = brightness_mux_out - 1;
     if (brightness_mux_out == 0) begin
-        channels[2] = 15;
+        channels[2] = 0;
     end
 end
 
@@ -256,11 +263,14 @@ initial begin
     channels[3] = 0;
 end
 
-
 always @(op_code) begin
     if (op_code == 1) begin
         color_mux_out = channels[3]; // default/initial value
         channels[2] = color_mux_out; 
+    end
+    else if (op_code[3] == 1 && color_mux_out != op_code[2:0]) begin
+        color_mux_out = op_code[2:0];
+        channels[2] = channels[0]; 
     end
     else if (op_code == 4) begin
         color_mux_out = channels[1];
@@ -314,8 +324,8 @@ module Strip_mux (
 
     always @(*) begin
         strip_out = channels[mode];
-        $display("Mode: %d",mode);
-        $display ("Rainbow: [%d, %d, %d]", rainbow_color[0][23:16], rainbow_color[0][15:8], rainbow_color[0][7:0]);
+        //$display("Mode: %d",mode);
+        //$display ("Rainbow: [%d, %d, %d]", rainbow_color[0][23:16], rainbow_color[0][15:8], rainbow_color[0][7:0]);
         
     end
 
@@ -362,8 +372,6 @@ module get_rainbow_color (
     reg [7:0][9:0][23:0] colors; 
 
     initial begin
-        //TODO: fill in colors
-        //colors[0] = 
         colors[0][0] = 24'b00001001_01000000_01110100;
         colors[0][1] = 24'b00111100_01101001_10010111;
         colors[0][2] = 24'b01011010_11011011_11111111;
@@ -470,7 +478,7 @@ module get_rainbow_color (
         colors[0][7] <= colors[0][6];
         colors[0][8] <= colors[0][7];
         colors[0][9] <= colors[0][8];
-        $display("cycle.");
+        //$display("cycle.");
     end
         
 endmodule
